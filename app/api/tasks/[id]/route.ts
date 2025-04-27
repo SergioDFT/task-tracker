1316fed3beb61425eda0ae136a0db068c85783c1
SergioDFT <sergio3004.2002@gmail.com>
2025-04-27 16:26:31 -0500

    Fix: avoiding get petition on [id]/route.ts

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -2,32 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
 
-export async function GET(req: NextRequest,  { params }: { params: { id: string }} ) {
-  const { id } = params;
-  const { userId } = await auth();
-
-  if (!userId) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
-
-  try {
-    const task = await prisma.task.findUnique({ where: { id } });
-
-    if (!task) {
-      return NextResponse.json({ error: "Task not found" }, { status: 404 });
-    }
-
-    if (task.userId !== userId) {
-      return NextResponse.json({ error: "Forbidden" }, { status: 403 });
-    }
-
-    return NextResponse.json(task);
-  } catch (err) {
-    console.error(err);
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-  }
-}
-
 export async function PUT(req: NextRequest, { params }: { params: { id: string }}) {
   const { id } = params;
   const { userId } = await auth();
